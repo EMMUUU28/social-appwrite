9c4d9f357489a0144c49a275ebc31f9ac7ddfa13
EMMUUU28 <francisgudinho1234@gmail.com>
2024-01-09 23:29:19 +0530

    routing

--- socialMatic/src/App.tsx
+++ socialMatic/src/App.tsx
@@ -1,9 +1,29 @@
+import { Route , Routes } from 'react-router-dom';
 import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import  Home from './_root/pages/Home';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
+
 const App = () =>{
     return (
-        <h1 className="text-3xl bg font-bold underline">
-        Hello world!
-      </h1>
+        <main className='flex h-screen' >
+            
+          <Routes>
+            {/* Public routes */}
+                <Route element={<AuthLayout/>} >
+                <Route path="/signin" element={<SigninForm/>}></Route>
+                <Route path="/signup" element={<SignupForm/>}></Route>
+                </Route>
+                
+
+            {/* Private routes */}
+            <Route element={<RootLayout/>}>
+            <Route index path='home' element={<Home/>}></Route>
+            </Route>
+          </Routes>
+        </main>
     )
 }
 
