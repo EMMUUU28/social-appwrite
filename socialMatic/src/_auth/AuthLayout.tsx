93165f752802122ef892a357fc3fc91b8e9753e1
EMMUUU28 <francisgudinho1234@gmail.com>
2024-01-11 23:06:03 +0530

    auth-pages

--- socialMatic/src/_auth/AuthLayout.tsx
+++ socialMatic/src/_auth/AuthLayout.tsx
@@ -1,8 +1,22 @@
-
+// import SignupForm from "./forms/SignupForm"
+import { Outlet, Navigate } from "react-router-dom"
 
 const AuthLayout = () => {
+
+  const isAuthenticated = false;
   return (
-    <div><h1>AuthLayout</h1></div>
+    <>
+    {isAuthenticated ? (
+    <Navigate to={"/"}/>
+    ) : ( 
+      <>
+    <section className="flex flex-1 justify-center items-center flex-col ">
+      <Outlet/>
+    </section>
+    <img src="/assets/images/side-img.svg" className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"></img>
+    </>
+    )}
+    </>
   )
 }
 
