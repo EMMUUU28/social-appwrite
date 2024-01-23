8a3818dba9cf5ab5e70cff58a55ed633612171d9
EMMUUU28 <francisgudinho1234@gmail.com>
2024-01-23 22:36:03 +0530

    addDatabase

--- socialMatic/src/lib/appwrite/api.ts
+++ socialMatic/src/lib/appwrite/api.ts
@@ -1,6 +1,8 @@
 import { INewUser } from "@/types";
-import { account } from "./config";
-import { ID } from 'appwrite';
+import { account, appwriteConfig, avatars, databases } from "./config";
+import { Account, ID } from 'appwrite';
+import { log } from "console";
+
 export async function createUserAccount(user:INewUser){
     try {
         const newAccount = await account.create(
@@ -11,9 +13,40 @@ export async function createUserAccount(user:INewUser){
             user.name,
             
         )
-        return newAccount;
+
+    const avatarUrl = avatars.getInitials(user.name)
+    const newUser = await saveUserToDB({
+        accountId:newAccount.$id,
+        name:newAccount.name,
+        email:newAccount.email,
+        username:user.username,
+        imageUrl:avatarUrl,
+    });
+        return newUser;
     } catch (error) {
         console.log(error);
         return error;
+    }
+}
+
+export async function saveUserToDB(user:
+    {
+    accountId:string,
+    email:string,
+    imageUrl:URL,
+    name:string,
+    username?:string
+
+}){
+    try {
+        const newUser = await databases.createDocument(
+            appwriteConfig.databaseid,
+            appwriteConfig.usersid,
+            ID.unique(),
+            user
+        )
+    } catch (error) {
+        console.log(error);
+        
     }
 }
--- socialMatic/src/lib/appwrite/config.ts
+++ socialMatic/src/lib/appwrite/config.ts
@@ -4,9 +4,18 @@ const VITE_PROJECT_ID='659c2b78c58bca89ed50';
 const VITE_PROJECT_URL='https://cloud.appwrite.io/v1';
 const VITE_APPWRITE_STORAGE_ID='65a0fb7e9948823f0ab3';
 const VITE_APPWRITE_DATABASE_ID='65a0fbf2bcbbc40ee76b'
+const VITE_APPWRITE_DATABASE_SAVES = '65afe6e957210e1e9734';
+const VITE_APPWRITE_DATABASE_USERS = '65afe6ce108ac70a2172';
+const VITE_APPWRITE_DATABASE_POSTS = '65afe6865788433f68e9'; 
+
 export const appwriteConfig = {
     projectId:VITE_PROJECT_ID,
-    url:VITE_PROJECT_URL
+    url:VITE_PROJECT_URL,
+    databaseid:VITE_APPWRITE_DATABASE_ID,
+    usersid:VITE_APPWRITE_DATABASE_USERS,
+    savesid:VITE_APPWRITE_DATABASE_SAVES,
+    postid:VITE_APPWRITE_DATABASE_POSTS,
+    storageid:VITE_APPWRITE_STORAGE_ID,
 
 }
 const client = new Client();
